87d07415a81f067d68ca3c6300ce05f45241fb5b
kkote <kaygray521@gmail.com>
2019-11-20 23:22:25 -0500

    Fixed Hamburger Closed

--- src/components/theme/Header/Hamburger/styles.js
+++ src/components/theme/Header/Hamburger/styles.js
@@ -7,7 +7,7 @@ export const Wrapper = styled.div`
 	display: none;
 	cursor: pointer;
 	transition: left 500ms cubic-bezier(0.6, 0.05, 0.28, 0.91);
-	position: absolute;
+	position: fixed;
 
 	@media (max-width: 960px) {
 		display: block;
